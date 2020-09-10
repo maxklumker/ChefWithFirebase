9bdca3d914c97554931685c4e556267fef4d556f
Luis <luiswoldu@googlemail.com>
2020-09-10 11:45:43 +0200

    Initial Commit

---
+++ Chef V2/Chef_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Chef_V2App.swift
+//  Chef V2
+//
+//  Created by Lewis Wall on 10.09.20.
+//
+
+import SwiftUI
+
+@main
+struct Chef_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chef V2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chef V2
+//
+//  Created by Lewis Wall on 10.09.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
